27e003a5d11b63ae5184e5217105d4621b1612e9
Fu Lim <fulim1130@gmail.com>
2024-03-11 18:30:20 +0800

    Initial a backend for gamehub to hide the key

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config(); // Load environment variables
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/api/platforms", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.rawg.io/api/platforms", {
+      params: {
+        key: process.env.API_KEY, // Access API key from environment variable
+      },
+    });
+    res.json(response.data.results);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
